36f777ef33a744c083393f5b38b3520ab7e2d01f
yah0834 <gksgud97@gmail.com>
2025-07-09 13:08:16 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 6.93 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+long long solution(long long n) {
+    long long answer = -1;
+    for(long long i = 1; i*i <= n; i++ ) {
+        if( i*i==n ) {
+            answer = (i+1)*(i+1);
+        }
+    }
+    return answer;
+}
