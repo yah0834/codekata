c3d1a5a85882e9adf9612ffd32948ebb73ed833b
yah0834 <gksgud97@gmail.com>
2025-08-05 20:42:21 +0900

    [level 1] Title: 음양 더하기, Time: 0.02 ms, Memory: 4.02 MB -BaekjoonHub

---
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> absolutes, vector<bool> signs) {
+    int answer = 0;
+
+    for (size_t i = 0; i < absolutes.size(); ++i) {
+        
+        answer += signs[i] ? absolutes[i] : -absolutes[i];
+    }
+
+    return answer;
+}
